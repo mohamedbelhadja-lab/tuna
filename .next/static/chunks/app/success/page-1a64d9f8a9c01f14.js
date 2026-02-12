055fee90c46e3442213d09a24e74156475f2d420
mohamedbelhadja-lab <mohamedbelhadja@gmail.com>
2026-02-12 11:09:38 +0000

    Add Supabase client

---
+++ .next/static/chunks/app/success/page-1a64d9f8a9c01f14.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[60],{7394:function(e,r,t){Promise.resolve().then(t.bind(t,9128))},9128:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return u}});var n=t(7437),o=t(2265),s=t(4033);function u(){let e=(0,s.useRouter)();return(0,o.useEffect)(()=>{let r=setTimeout(()=>{e.push("/playlist")},2e3);return()=>clearTimeout(r)},[e]),(0,n.jsxs)("main",{style:{padding:24,fontFamily:"sans-serif"},children:[(0,n.jsx)("h1",{children:"\uD83C\uDF89 Well done!"}),(0,n.jsx)("p",{children:"Your song has been submitted."})]})}},622:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(2265),o=Symbol.for("react.element"),s=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),u=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function f(e,r,t){var n,f={},l=null,c=null;for(n in void 0!==t&&(l=""+t),void 0!==r.key&&(l=""+r.key),void 0!==r.ref&&(c=r.ref),r)s.call(r,n)&&!i.hasOwnProperty(n)&&(f[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===f[n]&&(f[n]=r[n]);return{$$typeof:o,type:e,key:l,ref:c,props:f,_owner:u.current}}r.jsx=f,r.jsxs=f},7437:function(e,r,t){"use strict";e.exports=t(622)},4033:function(e,r,t){e.exports=t(290)}},function(e){e.O(0,[971,864,744],function(){return e(e.s=7394)}),_N_E=e.O()}]);
